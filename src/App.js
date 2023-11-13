e6ee1c50084d666ca132f9c950f65d4cfa954d7d
ThunderV01d <sidharthanil32@gmail.com>
2023-11-13 20:24:13 +0400

    detemplated

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, {useEffect, useState} from 'react';
+import './App.css';
+import useWebSocket from 'react-use-websocket';
+
+const wsurl = 'ws://127.0.0.1:8000';
+// const wsurl = 'wss://among-us-web-socket-server.glitch.me/';
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
