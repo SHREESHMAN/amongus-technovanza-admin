86c343d123b0cb52de5a2a1ef220e900fbf8b239
ThunderV01d <sidharthanil32@gmail.com>
2023-11-15 00:17:43 +0400

    major update

--- src/App.js
+++ src/App.js
@@ -2,15 +2,44 @@ import React, {useEffect, useState} from 'react';
 import './App.css';
 import useWebSocket from 'react-use-websocket';
 
-const wsurl = 'ws://127.0.0.1:8000';
-// const wsurl = 'wss://among-us-web-socket-server.glitch.me/';
+// const wsurl = 'ws://127.0.0.1:8000';
+const wsurl = 'wss://among-us-web-socket-server.glitch.me/';
+
+const Table = ({ tableData }) => {
+  if (Object.keys(tableData).length === 0) {
+    return <p>(No data available)</p>;
+  }
+
+  const headers = Object.keys(tableData[Object.keys(tableData)[0]]);
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          {headers.map((header) => (
+            <th key={header}>{header}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {Object.keys(tableData).map((rowKey) => (
+          <tr key={rowKey}>
+            {headers.map((header) => (
+              <td key={header}>{JSON.stringify(tableData[rowKey][header])}</td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
 
 function App() {
 
   //define states
   const [phaseMessage,setPhaseMessage] = useState("");
   const [time,setTime] = useState("");
-
+  const [tableData, setTableData] = useState({});
   //websocket connection
     const {sendJsonMessage,lastJsonMessage} = useWebSocket(wsurl,{
       onOpen: () => {
@@ -47,6 +76,19 @@ function App() {
             setPhaseMessage("Game is underway");
           }
         }
+        else if(lastJsonMessage.type==='broadcastTime'){
+          setTime(secondsToMMSS(lastJsonMessage.data));
+          if(phaseMessage==='Game has not yet started'){
+            setTime('');
+          }
+        }
+        else if(lastJsonMessage.type==='playerEvent'){
+          setTableData(lastJsonMessage.data);
+        }
+        else if(lastJsonMessage.type === 'endGame'){
+          setPhaseMessage("Game has not yet started");
+          setTime('');
+        }
     }
   }, [lastJsonMessage]);
 
@@ -65,13 +107,20 @@ function App() {
       type: 'endGame'
     })
   }
+  function startVoting(){
+    sendJsonMessage({
+      type: 'allowVoting'
+    })
+  }
   //markup
   return (
     <div className="App">
       <h1>Among Us IRL -TechnoVanza</h1>
       <h2>ADMIN PORTAL</h2>
-      <p>{phaseMessage}{phaseMessage==="Game has not yet started"?"":":"}{time}</p>
+      <p>{phaseMessage}{phaseMessage==="Game has not yet started"?"":": "}{time}</p>
+      <Table tableData={tableData} />
       <button id="force-end-game" onClick={endGame}>FORCE END GAME</button>
+      <button id="start-voting" onClick={startVoting}>EMERGENCY MEETING</button>
     </div>
   );
 }
